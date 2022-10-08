a186ccf4186b63121146a6246e87ef61095872b2
Aaron Stockdale <shadowstock76@gmail.com>
2022-10-08 02:27:10 -0400

    Got it working about halfway, just need some help on puctuation and frequency

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,6 @@
+from string import punctuation
+
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -5,9 +8,37 @@ STOP_WORDS = [
 ]
 
 
+
+
 def print_word_freq(file):
+    text_file = open(file)
+    data = text_file.read()
+    data_list = data.split('\n')
+    text_file.close()
+    """print (data.lower())"""
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    print(data_list)
+    
+    import string
+
+    clean_words = []
+    for word in data_list:
+        for letter in word:
+            if letter in string.punctuation:
+                word = word.replace(letter,"")
+        clean_words.append(word)
+
+    print (clean_words)
+
+    squeaky_words = []
+    for word in clean_words:
+        squeaky_words.append(word.lower())
+
+    print (", ".join(squeaky_words))
+
+"""So this gets the txt file into list of strings. It is all lower case and most
+of the punctuation is taken out (still having trouble with some commas). Still need
+to remove the stop words and count/print frequency"""
 
 
 if __name__ == "__main__":
